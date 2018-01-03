80b51880f146a21ebe710bcef59327d49404987f
tsiens <qina1020@gmail.com>
2018-01-03 12:28:35 +0800

    0103 升级django2.0

--- rail/urls.py
+++ rail/urls.py
@@ -1,25 +1,28 @@
 """rail URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.8/topics/http/urls/
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
 from django.views import static
-from django.conf.urls import include, url
 from django.contrib import admin
+from django.urls import path
+from django.conf.urls import url
 
-from web import views as web
+from web.views import *
+
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^station/(.+)', web.station),
-    url(r'^static/(?P<path>.*)$', static.serve, {'document_root': settings.STATIC_ROOT }, name='static'),
+    url('admin/', admin.site.urls),
+    url('station/(.+)/', station, name='station'),
+    url('static/(?P<path>.*)', static.serve, {'document_root': settings.STATIC_ROOT}, name='static'),
 ]
